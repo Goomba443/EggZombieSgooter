5ed89638b1431016753f7ee4211eac9a1afbc219
Goomba443 <isanchezq@campusdigitalfp.com>
2024-11-27 23:51:10 +0100

    metal plates got egged

--- Assets/Scripts/Egg.cs
+++ Assets/Scripts/Egg.cs
@@ -7,11 +7,31 @@ public class Egg : MonoBehaviour
 {
     private void OnCollisionEnter(Collision collision)
     {
-        Debug.Log("COLISIONOOOOOOO");
         if(collision.gameObject.CompareTag("Target"))
         {
             Debug.Log("hit " + collision.gameObject.name + "!");
+
+            CreateEggImpactEffect(collision);
             Destroy(gameObject);
         }
+
+        if (collision.gameObject.CompareTag("Wall"))
+        {
+            Debug.Log("hit a wall");
+            CreateEggImpactEffect(collision);
+            Destroy(gameObject);
+        }
+    }
+
+    void CreateEggImpactEffect(Collision objectHit)
+    {
+        ContactPoint contact = objectHit.contacts[0];
+        GameObject eggWhite = Instantiate(
+            GlobalReferences.Instance.eggImpactEffectPrefab,
+            contact.point,
+            Quaternion.LookRotation(contact.normal)
+            );
+
+        eggWhite.transform.SetParent(objectHit.gameObject.transform);
     }
 }
--- Assets/Scripts/Weapon.cs
+++ Assets/Scripts/Weapon.cs
@@ -4,26 +4,105 @@ using UnityEngine;
 
 public class Weapon : MonoBehaviour
 {
+    public Camera playerCamera;
+    public bool isShooting;
+    public bool readyToShoot;
+    bool allowReset = true;
+    public float shootingDelay = 2f;
+    public int bulletsPerBurst = 3;
+    public int burstEggsLeft;
+    public float spreadIntensity;
 
     public GameObject eggPrefab;
     public Transform eggSpawn;
     public float eggVelocity = 30;
     public float eggPrefabLifetime = 3f;
 
+    public enum ShootingMode
+    {
+        Single,
+        Burst,
+        Auto
+    }
+
+    public ShootingMode currentShootingMode;
+
+    private void Awake()
+    {
+        readyToShoot = true;
+        burstEggsLeft = bulletsPerBurst;
+    }
 
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Mouse0))
+        if (currentShootingMode == ShootingMode.Auto)
         {
+            isShooting = Input.GetKey(KeyCode.Mouse0);
+        }
+        else if (currentShootingMode == ShootingMode.Single || currentShootingMode == ShootingMode.Burst)
+        {
+            isShooting = Input.GetKey(KeyCode.Mouse0);
+        }
+
+        if (readyToShoot && isShooting)
+        {
+            burstEggsLeft = bulletsPerBurst;
             FireWeapon();
         }
     }
 
     private void FireWeapon()
     {
+        readyToShoot = false;
+
+        Vector3 shootingDirection = CalculateDirectionAndSpread().normalized;
+
         GameObject egg = Instantiate(eggPrefab, eggSpawn.position, Quaternion.identity);
-        egg.GetComponent<Rigidbody>().AddForce(eggSpawn.forward.normalized * eggVelocity, ForceMode.Impulse);
+        egg.transform.forward = shootingDirection;
+
+        egg.GetComponent<Rigidbody>().AddForce(shootingDirection * eggVelocity, ForceMode.Impulse);
         StartCoroutine(DestroyEgg(egg, eggPrefabLifetime));
+
+        if(allowReset)
+        {
+            Invoke(nameof(ResetShot), shootingDelay);
+            allowReset = false;
+        }
+
+        if(currentShootingMode == ShootingMode.Burst && burstEggsLeft > 1)
+        {
+            burstEggsLeft--;
+            Invoke(nameof(FireWeapon), shootingDelay);
+        }
+    }
+
+    private void ResetShot()
+    {
+        readyToShoot = true;
+        allowReset = true;
+    }
+
+    public Vector3 CalculateDirectionAndSpread()
+    {
+        Ray ray = playerCamera.ViewportPointToRay(new Vector3(0.5f, 0.5f, 0));
+        RaycastHit hit;
+
+        Vector3 targetPoint;
+        if(Physics.Raycast(ray, out hit))
+        { 
+            targetPoint = hit.point;
+        }
+        else
+        {
+            targetPoint = ray.GetPoint(100);
+        }
+
+        Vector3 direction = targetPoint - eggSpawn.position;
+
+        float x = UnityEngine.Random.Range(-spreadIntensity, spreadIntensity);
+        float y = UnityEngine.Random.Range(-spreadIntensity, spreadIntensity);
+
+        return direction + new Vector3(x, y, 0);
     }
 
     private IEnumerator DestroyEgg(GameObject egg, float delay)
